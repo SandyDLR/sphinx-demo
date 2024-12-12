7f4709ee37e16d00ca5ae29c04c45b6410c4d5a6
saelric <saelric@gmail.com>
2024-12-12 17:30:13 -0600

    fixes

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# Install Sphinx if not installed
+# Install Sphinx
+pip3 install --upgrade pip
 pip3 install sphinx
+
+# Add the pip user binary directory to PATH
+export PATH="$HOME/.local/bin:$PATH"
+
+# Debugging step to verify sphinx-build is in PATH
+which sphinx-build
 
 # Build the Sphinx documentation
 sphinx-build -b html source build/html
